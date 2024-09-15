af21bb1db2cf3f6a06557f8dafb610e48dd5eced
jaikrishna612 <jaikrishnareddy2002@gmail.com>
2024-09-15 13:41:23 +0530

    assignmet

--- src/pages/CompanyInfo.jsx
+++ src/pages/CompanyInfo.jsx
@@ -1,6 +1,11 @@
 const CompanyInfo = () => {
   return (
-    <div>CompanyInfo</div>
+    <div className="container">
+      <p className="display-6 text-dark mt-3 ms-0"><strong>Company</strong> : Geeksynergy Technologies Pvt Ltd</p>
+      <p className="display-6 text-dark mt-3 ms-0" > <strong>Address</strong>: Sanjayanagar, Benguluru-56</p>
+      <p className="display-6 text-dark mt-3 ms-0"><strong>Phone</strong>: XXXXXXXX09</p>
+      <p className="display-6 text-dark mt-3 ms-0"><strong>Email</strong>: XXXXXX@gmail.com</p>
+    </div>
   )
 }
 export default CompanyInfo
